fa365e9ec751d346127d3ed095969132e05b5708
Suphakit P. <me@suphakit.net>
2025-04-19 17:49:31 +0700

    feat<components>: added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full py-4 fixed left-0 bottom-0 text-center bg-cat-crust text-cat-text">
+    &copy; 2025 <a href="https://suphakit.net" target="_blank">Suphakit P.</a> All Right Reserved.
+  </div>
+</template>
