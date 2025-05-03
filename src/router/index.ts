fdbac20492a390f886f9fcb72a37960b5a86fffe
Suphakit P. <me@suphakit.net>
2025-05-04 00:16:50 +0700

    chore<route>: added new route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ShortView from '../views/ShortView.vue'
+import CheckView from '@/views/CheckView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/check',
+      name: 'check',
+      component: CheckView,
     },
     {
       path: '/sh/:slug',
